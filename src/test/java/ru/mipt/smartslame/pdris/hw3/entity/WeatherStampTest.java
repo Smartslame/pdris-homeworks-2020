b1ff3a7174359c2094fee7513284ed26d74bb409
Yakushkin Alexander <smartslame@yandex.ru>
2020-12-21 12:09:30 +0300

    add empty test

---
+++ src/test/java/ru/mipt/smartslame/pdris/hw3/entity/WeatherStampTest.java
@@ -0,0 +1,34 @@
+package ru.mipt.smartslame.pdris.hw3.entity;
+
+import static org.junit.Assert.*;
+
+public class WeatherStampTest {
+
+    @org.junit.Test
+    public void getCity() {
+    }
+
+    @org.junit.Test
+    public void getMaxTemp() {
+    }
+
+    @org.junit.Test
+    public void getMinTemp() {
+    }
+
+    @org.junit.Test
+    public void getAvgTemp() {
+    }
+
+    @org.junit.Test
+    public void getMaxWindSpeed() {
+    }
+
+    @org.junit.Test
+    public void getAvgVisibility() {
+    }
+
+    @org.junit.Test
+    public void getDate() {
+    }
+}
